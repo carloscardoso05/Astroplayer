536e38c56d6ce58eecb9ad41e448159ab0dc9a28
Carlos Silva <cvcardos5@gmail.com>
2025-03-13 15:34:36 -0300

    atualiza e implementa lista navegavel

--- src/main/java/prog2/util/ListaNavegavel.java
+++ src/main/java/prog2/util/ListaNavegavel.java
@@ -1,38 +1,105 @@
 package prog2.util;
 
-import java.util.List;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.NoSuchElementException;
 
-public class ListaNavegavel<T> {
+public class ListaNavegavel<T> extends ArrayList<T> {
     private int indexAtual;
-    List<T> elementos;
 
-    T getAtual() {
-        return elementos.get(indexAtual);
+    public T getAtual() {
+        return get(indexAtual);
     }
 
-    T getProximo() {
-        if (indexAtual + 1 < elementos.size()) {
-            return elementos.get(indexAtual + 1);
+    public boolean temProximo() {
+        return indexAtual + 1 < size();
+    }
+
+    public boolean temAnterior() {
+        return indexAtual > 0;
+    }
+
+    public T irParaProximo() {
+        if (!temProximo()) {
+            throw new NoSuchElementException("Não há próximo elemento");
         }
-        return elementos.getFirst();
+        return get(++indexAtual);
     }
 
-    T getAnterior() {
-        if (indexAtual > 0) {
-            return elementos.get(indexAtual - 1);
+    public T irParaAnterior() {
+        if (!temAnterior()) {
+            throw new NoSuchElementException("Não há elemento anterior");
         }
-        return elementos.getLast();
+        return get(--indexAtual);
     }
 
-    T irParaProximo() {
-        return null;
+    public T getProximo() {
+        if (!temProximo()) {
+            throw new NoSuchElementException("Não há próximo elemento");
+        }
+        return get(indexAtual + 1);
     }
 
-    T irParaAnterior() {
-        return null;
+    public T getAnterior() {
+        if (!temAnterior()) {
+            throw new NoSuchElementException("Não há elemento anterior");
+        }
+        return get(indexAtual - 1);
     }
 
-    List<T> getTodos() {
-        return null;
+    @Override
+    public void add(int index, T element) {
+        super.add(index, element);
+        if (index <= indexAtual) {
+            indexAtual++;
+        }
     }
+
+    @Override
+    public boolean addAll(int index, Collection<? extends T> c) {
+        boolean added = addAll(index, c);
+        if (added && index <= indexAtual) {
+            indexAtual += c.size();
+        }
+        return added;
+    }
+
+    @Override
+    public T remove(int index) {
+        final T removedElement = super.remove(index);
+        if (index < indexAtual) {
+            indexAtual--;
+        } else if (index == indexAtual) {
+            if (indexAtual == size()) {
+                indexAtual = Math.max(0, indexAtual - 1);
+            }
+        }
+        return removedElement;
+    }
+
+    @Override
+    public boolean remove(Object o) {
+        int index = indexOf(o);
+        if (index >= 0) {
+            remove(index);
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean removeAll(Collection<?> c) {
+        boolean modified = false;
+        for (Object o : c) {
+            modified |= remove(o);
+        }
+        return modified;
+    }
+
+    @Override
+    public void clear() {
+        super.clear();
+        indexAtual = 0;
+    }
+
 }
