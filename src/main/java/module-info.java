eb2420cb584457b0bc0f5dafa00b1c9cd6bdc685
Carlos Vitor Cardoso da Silva <cvcardos5@gmail.com>
2025-03-14 08:47:28 -0300

    adiciona sqlite

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -3,6 +3,7 @@ module prog2.astroplayer {
     requires javafx.fxml;
     requires javafx.media;
     requires static lombok;
+    requires java.sql;
 
 
     opens prog2.astroplayer.ui to javafx.fxml;
---
+++ src/main/java/prog2/astroplayer/db/DB.java
@@ -0,0 +1,17 @@
+package prog2.astroplayer.db;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DB {
+    private static Connection connection;
+    private static final String url = "jdbc:sqlite:test.db";
+
+    public static Connection getConnection() throws SQLException {
+        if (connection == null) {
+            connection = DriverManager.getConnection(url);
+        }
+        return connection;
+    }
+}
